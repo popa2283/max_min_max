978259bca15e08d8009ec292f4b3491cd7dce297
popa2283 <127163765+popa2283@users.noreply.github.com>
2024-04-30 23:54:48 +0300

    Добавьте файлы проекта.

---
+++ max_min_max.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	vector<int> nams = { -4,-2,-1,-1,0,1,2,3 };
+	for (size_t i = 0; i < nams.size(); i++)
+	{
+		if (nams[i] < 0) {
+			nams[i] = nams[i] * -1;
+		}
+	}
+	sort(begin(nams), end(nams));
+	for (size_t i = 0; i < nams.size(); i++)
+	{
+		if (i==nams.size()-1) {
+			cout << nams[i] << endl;
+		}
+	}
+}
+
